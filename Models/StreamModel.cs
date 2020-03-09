dae18f36aea7b39b29e0ec7ee715bceae9a583d7
Joey Hendrich <jlh1100@yahoo.com>
2020-03-09 03:38:58 -0500

    ToDo: Make stream pic a link

--- Models/StreamModel.cs
+++ Models/StreamModel.cs
@@ -7,6 +7,7 @@ namespace BorisGangBot_Mk2.Models
     public class StreamModel
     {
         public string Stream { get; set; }
+        public string Avatar { get; set; }
         public bool Live { get; set; }
         public string Link { get; set; }
     }
--- Modules/LiveModule.cs
+++ Modules/LiveModule.cs
@@ -10,6 +10,7 @@ using TwitchLib;
 using TwitchLib.Api;
 using TwitchLib.Api.Helix;
 using TwitchLib.Api.Helix.Models.Streams;
+using TwitchLib.Api.Helix.Models.Users;
 
 namespace BorisGangBot_Mk2.Modules
 {
@@ -19,6 +20,7 @@ namespace BorisGangBot_Mk2.Modules
         private static TwitchAPI api;
         private readonly IConfigurationRoot _config;
         private GetStreamsResponse response = new GetStreamsResponse();
+        private GetUsersResponse u_response = new GetUsersResponse();
 
         public LiveModule(IConfigurationRoot config)
         {
@@ -30,6 +32,7 @@ namespace BorisGangBot_Mk2.Modules
         public async Task Live()
         {
             List<string> streams = new List<string>();
+            List<StreamModel> streams_response = new List<StreamModel>();
 
             api = new TwitchAPI();
             api.Settings.ClientId = _config["tokens:tw_cID"];
@@ -42,9 +45,53 @@ namespace BorisGangBot_Mk2.Modules
                 count++;
             }
 
-            response = await api.Helix.Streams.GetStreamsAsync(null, null, 50, null, null, "all", null, streams);
+            response = await api.Helix.Streams.GetStreamsAsync(null, null, 5, null, null, "all", null, streams); // Add streams list back after testing
+            streams.Clear();
 
-            await ReplyAsync(streams[0], false);
+            count = 0;
+            while (count < response.Streams.Length)
+            {
+                streams.Add(response.Streams[count].UserName);
+                count++;
+            }
+
+            u_response = await api.Helix.Users.GetUsersAsync(null, streams);
+
+            count = 0;
+            while (count < response.Streams.Length)
+            {
+                StreamModel x = new StreamModel()
+                {
+                    Stream = response.Streams[count].UserName,
+                    Avatar = u_response.Users[count].ProfileImageUrl,
+                    Live = true,
+                };
+                x.Link = $"https://www.twitch.tv/{x.Stream}";
+
+                streams_response.Add(x);
+                count++;
+            }
+
+            if (streams_response.Count == 0)
+            {
+                await ReplyAsync("No members of Boris Gang are currently live.", false);
+            } else
+            {
+                foreach (StreamModel s in streams_response)
+                {
+                    var builder = new EmbedBuilder()
+                    {
+                        Color = new Color(255, 0, 0),
+                        Title = $"{s.Stream} is Live!",
+                        ImageUrl = s.Avatar,
+                        Url = s.Link
+                    };
+
+                    Console.WriteLine(s.Avatar);
+                    await ReplyAsync("", false, builder.Build());
+
+                }
+            }
         }
     }
 }
