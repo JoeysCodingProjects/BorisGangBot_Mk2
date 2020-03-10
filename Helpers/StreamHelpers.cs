74f2b6b8938c2f42ffbc6bfa16761b6fe90961a3
Joey Hendrich <jlh1100@yahoo.com>
2020-03-10 13:14:43 -0500

    StreamsHelpers.cs -> StreamHelpers.cs | Added BG_CreateStreamerEmbeds func

--- Helpers/StreamHelpers.cs
+++ Helpers/StreamHelpers.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using BorisGangBot_Mk2.Models;
+using Discord;
 using TwitchLib;
 using TwitchLib.Api;
 using TwitchLib.Api.Helix;
@@ -12,10 +13,11 @@ using TwitchLib.Api.Helix.Models.Users;
 
 namespace BorisGangBot_Mk2.Helpers
 {
-    public class StreamsHelpers
+    public class StreamHelpers
     {
         // Returns a list of StreamModels for a given list of stream names
         // Params: TwitchAPI, List<string>
+        // Returns: List<StreamModel>
         public async Task<List<StreamModel>> BG_GetLiveStreams(TwitchAPI api, List<string> s)
         {
             // Key = GameID, Value = Game Name
@@ -26,10 +28,18 @@ namespace BorisGangBot_Mk2.Helpers
             GetUsersResponse u_response = new GetUsersResponse();
 
             List<string> g_IDs = new List<string>();
+            List<string> s_live = new List<string>();
             List<StreamModel> streamModels = new List<StreamModel>();
 
             s_response = await api.Helix.Streams.GetStreamsAsync(null, null, s.Count, null, null, "all", null, s);
-            u_response = await api.Helix.Users.GetUsersAsync(null, s);
+            foreach (Stream x in s_response.Streams)
+            {
+                // Only get the avatars of streams that are live
+                // Not doing this results in getting all streamer
+                // avatars and assigning them to the wrong streamer
+                s_live.Add(x.UserName);
+            }
+            u_response = await api.Helix.Users.GetUsersAsync(null, s_live);
 
             if (s_response.Streams.Length == 0) { return streamModels; }
 
@@ -51,7 +61,7 @@ namespace BorisGangBot_Mk2.Helpers
 
             g_response = await api.Helix.Games.GetGamesAsync(g_IDs, null);
             
-            foreach (Game x in g_response.Games)
+            foreach (TwitchLib.Api.Helix.Models.Games.Game x in g_response.Games)
             {
                 g_dictionary.TryAdd(x.Id, x.Name);
             }
@@ -64,6 +74,47 @@ namespace BorisGangBot_Mk2.Helpers
             }
 
             return streamModels;
+        }
+
+        // Creates the EmbedBuilder(s) for all given streams
+        // Params: List<StreamModel>
+        // Returns: List<EmbedBuilder>
+        public List<EmbedBuilder> BG_CreateStreamerEmbeds(List<StreamModel> s)
+        {
+            List<EmbedBuilder> eb_list = new List<EmbedBuilder>();
+
+            foreach (StreamModel sm in s)
+            {
+                var a = new EmbedAuthorBuilder()
+                {
+                    Name = sm.Stream,
+                    IconUrl = sm.Avatar
+                };
+                var eb = new EmbedBuilder()
+                {
+                    Author = a,
+                    Color = new Color(0, 200, 0),
+                    ThumbnailUrl = sm.Avatar,
+                    Title = sm.Title,
+                    Url = sm.Link
+                };
+                eb.AddField(x =>
+                {
+                    x.IsInline = true;
+                    x.Name = "**Playing:**";
+                    x.Value = sm.Game;
+                });
+                eb.AddField(x =>
+                {
+                    x.IsInline = true;
+                    x.Name = "**Viewers:**";
+                    x.Value = sm.Viewers;
+                });
+
+                eb_list.Add(eb);
+            }
+
+            return eb_list;
         }
     }
 }
--- Modules/LiveModule.cs
+++ Modules/LiveModule.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using BorisGangBot_Mk2.Models;
+using BorisGangBot_Mk2.Helpers;
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
@@ -20,9 +21,6 @@ namespace BorisGangBot_Mk2.Modules
     {
         private static TwitchAPI api;
         private readonly IConfigurationRoot _config;
-        private GetStreamsResponse response = new GetStreamsResponse();
-        private GetUsersResponse u_response = new GetUsersResponse();
-        private GetGamesResponse g_response = new GetGamesResponse();
 
         public LiveModule(IConfigurationRoot config)
         {
@@ -33,9 +31,10 @@ namespace BorisGangBot_Mk2.Modules
         [Summary("Lists all currently streaming members of Boris Gang.")]
         public async Task Live()
         {
+            List<EmbedBuilder> eb_list = new List<EmbedBuilder>();
             List<string> streams = new List<string>();
-            List<StreamModel> streams_response = new List<StreamModel>();
-            List<string> games = new List<string>();
+            List<StreamModel> s_response = new List<StreamModel>();
+            StreamHelpers streamHelpers = new StreamHelpers();
 
             api = new TwitchAPI();
             api.Settings.ClientId = _config["tokens:tw_cID"];
@@ -48,80 +47,19 @@ namespace BorisGangBot_Mk2.Modules
                 count++;
             }
 
-            response = await api.Helix.Streams.GetStreamsAsync(null, null, 5, null, null, "all", null, streams); // Add streams list back after testing
-            streams.Clear();
+            s_response = await streamHelpers.BG_GetLiveStreams(api, streams);
 
-            count = 0;
-            while (count < response.Streams.Length)
-            {
-                streams.Add(response.Streams[count].UserName);
-                count++;
-            }
-
-            u_response = await api.Helix.Users.GetUsersAsync(null, streams);
-
-            count = 0;
-            while (count < response.Streams.Length)
-            {
-                StreamModel x = new StreamModel()
-                {
-                    Stream = response.Streams[count].UserName,
-                    Avatar = u_response.Users[count].ProfileImageUrl,
-                    Live = true,
-                    Title = response.Streams[count].Title,
-                    Game = response.Streams[count].GameId,
-                    Viewers = response.Streams[count].ViewerCount
-                };
-                x.Link = $"https://www.twitch.tv/{x.Stream}";
-
-                streams_response.Add(x);
-                games.Add(x.Game);
-                count++;
-            }
-
-            g_response = await api.Helix.Games.GetGamesAsync(games, null);
-            for (int z = 0; z < g_response.Games.Length; z++)
-            {
-                streams_response[z].Game = g_response.Games[z].Name;
-            }
-
-            if (streams_response.Count == 0)
+            if (s_response.Count == 0)
             {
                 await ReplyAsync("No members of Boris Gang are currently live.", false);
-            } else
+            }
+            else
             {
-                foreach (StreamModel s in streams_response)
+                eb_list = streamHelpers.BG_CreateStreamerEmbeds(s_response);
+
+                foreach (EmbedBuilder x in eb_list)
                 {
-                    var a_builder = new EmbedAuthorBuilder()
-                    {
-                        Name = s.Stream,
-                        IconUrl = s.Avatar
-                    };
-
-                    var builder = new EmbedBuilder()
-                    {
-                        Color = new Color(255, 0, 0),
-                        Title = s.Title,
-                        ThumbnailUrl = s.Avatar,
-                        Url = s.Link,
-                        Author = a_builder
-                    };
-
-                    builder.AddField(x=>
-                    {
-                        x.IsInline = true;
-                        x.Name = "**Playing:**";
-                        x.Value = s.Game;
-                    });
-
-                    builder.AddField(x =>
-                    {
-                        x.IsInline = true;
-                        x.Name = "**Viewers**";
-                        x.Value = s.Viewers;
-                    });
-
-                    await ReplyAsync("", false, builder.Build());
+                    await ReplyAsync("", false, x.Build());
                 }
             }
         }
