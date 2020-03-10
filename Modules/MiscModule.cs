26367e225948059232d50c6a16d058efa68629cd
Joey Hendrich <jlh1100@yahoo.com>
2020-03-10 13:24:09 -0500

    Added MiscModule.cs for misc commands

---
+++ Modules/MiscModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace BorisGangBot_Mk2.Modules
+{
+    public class MiscModule : ModuleBase<SocketCommandContext>
+    {
+        [Name("source")]
+        [Summary("Gives a link to Boris Gang Bot's source code.")]
+        public async Task SourceAsync()
+        {
+            await ReplyAsync($"");
+        }
+    }
+}
