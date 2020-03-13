91d0da4e1726b24134cfc01b27be87aae093d2be
Joey Hendrich <jlh1100@yahoo.com>
2020-03-13 17:29:45 -0500

    Added new files as I work towards music capabilities

---
+++ Modules/BotAdminModules/ConfigModule.cs
@@ -0,0 +1,13 @@
+﻿using Discord.Commands;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BorisGangBot_Mk2.Modules.BotAdminModules
+{
+    public class ConfigModule : ModuleBase<SocketCommandContext>
+    {
+        private readonly IConfigurationRoot _config;
+    }
+}
