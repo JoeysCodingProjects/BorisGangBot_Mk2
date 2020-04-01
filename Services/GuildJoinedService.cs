0a4549455903f0213ba458d1cfb900c388a0ae05
Joey Hendrich <jlh1100@yahoo.com>
2020-03-31 22:08:56 -0500

    Added GuildJoinedService to send message whenever joining a new guild

---
+++ Services/GuildJoinedService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using BorisGangBot_Mk2.Models;
+using Discord;
+using Discord.WebSocket;
+using Newtonsoft.Json;
+
+namespace BorisGangBot_Mk2.Services
+{
+    public class GuildJoinedService
+    {
+        private readonly DiscordSocketClient _discord;
+        private readonly IServiceProvider _provider;
+
+        public GuildJoinedService(
+            DiscordSocketClient discord,
+            IServiceProvider provider)
+        {
+            _discord = discord;
+            _provider = provider;
+
+            _discord.JoinedGuild += OnJoinedGuildAsync;
+        }
+
+        private Task OnJoinedGuildAsync(SocketGuild guild)
+        {
+
+            return guild.DefaultChannel.SendMessageAsync("Thwanks for invwiting me here UwU, pwease send **;help** for a list of comwands my mwasta has given me UwU", false);
+        }
+    }
+}
