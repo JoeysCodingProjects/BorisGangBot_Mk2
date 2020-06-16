64d4908f3682aa7c2ae8a9001db8988f3f82d4a7
Joey Hendrich <jlh1100@yahoo.com>
2020-06-16 11:51:31 -0500

    Now based on twitch user ID, not user name

--- Helpers/StreamerFileHelper.cs
+++ Helpers/StreamerFileHelper.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using YamlDotNet.Serialization;
 using BorisGangBot_Mk2.Services.LiveStreamMono;
 using Microsoft.Extensions.Configuration;
+using TwitchLib.Api;
 
 namespace BorisGangBot_Mk2.Helpers
 {
@@ -34,12 +35,14 @@ namespace BorisGangBot_Mk2.Helpers
 
             VerifyStreamerHelper verifyStreamer = new VerifyStreamerHelper(_lsms);
 
-            if (await verifyStreamer.TryVerifyStreamerAsync(streamer) == false)
+            string streamerId = await verifyStreamer.TryVerifyStreamerAsync(streamer);
+            if (streamerId == null)
                 return -1;
 
             Deserializer deserializer = new Deserializer();
             Serializer serializer = new Serializer();
             string result;
+            string result2;
 
 
             using (StreamReader reader = File.OpenText("./Streamers.yml"))
@@ -48,11 +51,21 @@ namespace BorisGangBot_Mk2.Helpers
                 reader.Close();
             }
 
+            using (StreamReader reader = File.OpenText("./Streamids.yml"))
+            {
+                result2 = await reader.ReadToEndAsync();
+                reader.Close();
+            }
+
             List<string> streamslist = deserializer.Deserialize<List<string>>(result);
+            Dictionary<string, string> streamsIdDict = deserializer.Deserialize<Dictionary<string, string>>(result2);
 
-
+            streamsIdDict.Add(s, streamerId);
             streamslist.Add(s);
+
+            object dictfinal = serializer.Serialize(streamsIdDict);
             object listfinal = serializer.Serialize(streamslist);
+            await File.WriteAllTextAsync("./Streamids.yml", dictfinal.ToString());
             await File.WriteAllTextAsync("./Streamers.yml", listfinal.ToString());
 
             return 1;
@@ -74,7 +87,11 @@ namespace BorisGangBot_Mk2.Helpers
             Deserializer deserializer = new Deserializer();
             Serializer serializer = new Serializer();
             string result;
+            string result2;
             string s = streamer.ToLower();
+
+            if (!(_lsms.StreamList.Contains(s)))
+                return false;
 
             using (StreamReader reader = File.OpenText("./Streamers.yml"))
             {
@@ -82,13 +99,21 @@ namespace BorisGangBot_Mk2.Helpers
                 reader.Close();
             }
 
+            using (StreamReader reader = File.OpenText("./Streamids.yml"))
+            {
+                result2 = await reader.ReadToEndAsync();
+                reader.Close();
+            }
+
             List<string> streamslist = deserializer.Deserialize<List<string>>(result);
+            Dictionary<string, string> streamsIdDict = deserializer.Deserialize<Dictionary<string, string>>(result2);
 
-            if (!(streamslist.Contains(s)))
-                return false;
-
+            streamsIdDict.Remove(s);
             streamslist.Remove(s);
+
+            object dictfinal = serializer.Serialize(streamsIdDict);
             object listfinal = serializer.Serialize(streamslist);
+            await File.WriteAllTextAsync("./Streamids.yml", dictfinal.ToString());
             await File.WriteAllTextAsync("./Streamers.yml", listfinal.ToString());
 
             return true;
